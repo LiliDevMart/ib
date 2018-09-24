0437912522c43c0915eda53af8fb613df3162c4f
magali <lilidevmart@gmail.com>
2018-09-24 11:08:17 +0200

    Premier commit Branche master

---
+++ src/com/ib/app/App.java
@@ -0,0 +1,14 @@
+package com.ib.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Projet1Git : version 0.1");
+
+	}
+	
+	public void fonction1() {
+		System.out.println("Fonction 1");
+	}
+
+}
